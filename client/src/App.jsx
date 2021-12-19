bd91dd7c3dcf5d342f15ad89ec72424f07323f03
Pavel Krokhmal <pavel.krokhmal.mn@outlook.com>
2021-12-19 20:52:08 +0200

    implemented ChannelContainer.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {StreamChat} from 'stream-chat'
 import {Chat} from 'stream-chat-react'
 import {cookies} from './utils'
 
 import {ChannelContainer, ChannelListContainer, Auth} from './components'
 
+import 'stream-chat-react/dist/css/index.css'
 import './App.css'
 
 const client = StreamChat.getInstance(process.env.REACT_APP_STREAM_API_KEY)
@@ -23,14 +24,27 @@ if (authToken) {
 }
 
 const App = () => {
+  const [createType, setCreateType] = useState('')
+  const [isCreating, setIsCreating] = useState(false)
+  const [isEditing, setIsEditing] = useState(false)
+
   if (!authToken)
     return <Auth/>
 
   return (
     <div className='app__wrapper'>
       <Chat client={client} theme='team light'>
-        <ChannelListContainer/>
-        <ChannelContainer/>
+        <ChannelListContainer isCreating={isCreating}
+                              setIsCreating={setIsCreating}
+                              setCreateType={setCreateType}
+                              setIsEditing={setIsEditing}
+        />
+        <ChannelContainer isCreating={isCreating}
+                          setIsCreating={setIsCreating}
+                          isEditing={isEditing}
+                          setIsEditing={setIsEditing}
+                          createType={createType}
+        />
       </Chat>
     </div>
   )
