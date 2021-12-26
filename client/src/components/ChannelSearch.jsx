a7725124cd6509aeb5d14d30d27d145ce01e3e9b
Pavel Krokhmal <pavel.krokhmal.mn@outlook.com>
2021-12-26 11:05:35 +0200

    edit channel & search

--- client/src/components/ChannelSearch.jsx
+++ client/src/components/ChannelSearch.jsx
@@ -2,13 +2,39 @@ import React, {useState, useEffect} from 'react'
 import {useChatContext} from 'stream-chat-react'
 
 import {SearchIcon} from '../assets'
+import {ResultsDropdown} from './'
 
-const ChannelSearch = () => {
+const ChannelSearch = ({setToggleContainer}) => {
+  const {client, setActiveChannel} = useChatContext()
   const [query, setQuery] = useState('')
   const [loading, setLoading] = useState(false)
+  const [teamChannels, setTeamChannels] = useState([])
+  const [directChannels, setDirectChannels] = useState([])
+
+  useEffect(() => {
+    if (!query) {
+      setTeamChannels([])
+      setDirectChannels([])
+    }
+  }, [query])
+
   const getChannels = async (text) => {
     try {
-      // TODO: fetch channels
+      const channelResponse = client.queryChannels({
+        type: 'team',
+        name: {$autocomplete: text},
+        members: {$in: [client.userID]}
+      })
+      const userResponse = client.queryUsers({
+        id: {$ne: client.userID},
+        name: {$autocomplete: text}
+      })
+
+      const [channels, {users}] = await Promise.all([channelResponse, userResponse])
+
+      if (channels.length) setTeamChannels(channels)
+      if(users.length) setDirectChannels(users)
+
     } catch (error) {
       setQuery('')
     }
@@ -23,6 +49,11 @@ const ChannelSearch = () => {
     getChannels(event.target.value)
   }
 
+  const setChannel = (channel) => {
+    setQuery('')
+    setActiveChannel(channel)
+  }
+
   return (
     <div className='channel-search__container'>
       <div className='channel-search__input__wrapper'>
@@ -35,6 +66,14 @@ const ChannelSearch = () => {
                onChange={onSearch}
                type='text'/>
       </div>
+      {query && (
+        <ResultsDropdown teamChannels={teamChannels}
+                         directChannels={directChannels}
+                         setQuery={setQuery}
+                         setChannel={setChannel}
+                         setToggleContainer={setToggleContainer}
+                         loading={loading}/>
+      )}
     </div>
   )
 }
